761be428ff50958791c7955c76438e1e64a42fb4
soubhagyasethy <ssethy017@gmail.com>
2023-03-18 02:14:51 +0530

    feat: implement home page

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Cardsdata from "./CardsData";
+import "./style.css";
 
 const Cards = () => {
-  return <div>Cards</div>;
+  const [data, setData] = useState(Cardsdata);
+  console.log(data);
+  return (
+    <div className="container mt-3 ">
+      <h2 className="text-center">Add to Cart Project</h2>
+
+      <div className="row d-flex justify-content-center align-items-center">
+        {data.map((element, id) => {
+          return (
+            <>
+              <Card
+                style={{ width: "15rem", border: "none" }}
+                className="mx-2 mt-4 card_style"
+              >
+                <Card.Img
+                  variant="top"
+                  src={element.imgdata}
+                  style={{ height: "11rem" }}
+                />
+                <Card.Body>
+                  <Card.Title>{element.rname}</Card.Title>
+                  <Card.Text>Price : ₹ {element.price}</Card.Text>
+                  <div className="button_div d-flex justify-content-center">
+                    <Button variant="primary" className="col-lg-12">
+                      Add to Cart
+                    </Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
